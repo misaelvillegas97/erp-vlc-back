2ce3a8c04309945f4ff2f6f4c15ab29fecce790d
David Misael Villegas Sandoval <david.misa97@gmail.com>
2025-08-05 04:39:55 -0400

    feat(audit): enhance audit interceptor with unique request ID and improved logging

--- src/modules/audit/audit.module.ts
+++ src/modules/audit/audit.module.ts
@@ -1,16 +1,18 @@
-import { Module }         from '@nestjs/common';
-import { TypeOrmModule }  from '@nestjs/typeorm';
-import { AuditLogEntity } from './domain/entities/audit-log.entity';
-import { AuditService }   from './services/audit.service';
+import { Module }           from '@nestjs/common';
+import { TypeOrmModule }    from '@nestjs/typeorm';
+import { APP_INTERCEPTOR }  from '@nestjs/core';
+import { AuditLogEntity }   from './domain/entities/audit-log.entity';
+import { AuditService }     from './services/audit.service';
+import { AuditInterceptor } from '@core/interceptors/audit.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AuditLogEntity])],
   providers: [
     AuditService,
-    // {
-    //   provide: APP_INTERCEPTOR,
-    //   useClass: AuditInterceptor,
-    // },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AuditInterceptor,
+    },
   ],
   exports: [AuditService],
 })
